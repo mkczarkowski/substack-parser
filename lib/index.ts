a5735943318ffdb687094ffd65754fb1a8207e9a
“mkczarkowski” <marcinczarkowski@onet.eu>
2022-08-28 20:07:19 +0200

    chore: adjust nodemon config

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,9 @@
 import puppeteer from 'puppeteer'
 
 async function start (): Promise<void> {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    headless: false
+  })
   const page = await browser.newPage()
 
   await page.goto('https://substack.com/sign-in?redirect=%2F')
