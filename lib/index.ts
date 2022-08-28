ec55c1795db18044c1b29178381f8ca8f47c1319
“mkczarkowski” <marcinczarkowski@onet.eu>
2022-08-28 20:43:18 +0200

    feat: access writer dashboard

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,31 @@
+import 'dotenv/config'
 import puppeteer from 'puppeteer'
 
-async function start (): Promise<void> {
+async function start(): Promise<void> {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: false,
+    defaultViewport: null
   })
   const page = await browser.newPage()
 
   await page.goto('https://substack.com/sign-in?redirect=%2F')
+  await page.click('.substack-login__login-option')
+
+
+  const emailInputHandle = await page.$('input[name="email"]')
+  const passwordInputHandle = await page.$('input[name="password"]')
+  await emailInputHandle?.type(process.env.SUBSTACK_LOGIN as string)
+  await passwordInputHandle?.type(process.env.SUBSTACK_PASSWORD as string)
+
+  await page.click('.substack-login__go-button')
+  await page.waitForNavigation();
+
+
+  const [button] = await page.$x("//button[contains(text(), 'Dashboard')]");
+
+  if (button) {
+    await button.click();
+  }
 }
 
 void start()
