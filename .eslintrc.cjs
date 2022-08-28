6377d2275327e13cdfe0d7269a167c9b6f8bcb38
“mkczarkowski” <marcinczarkowski@onet.eu>
2022-08-28 20:02:00 +0200

    chore: change eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,15 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: 'xo',
-	overrides: [
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	rules: {
-	},
-};
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: 'standard-with-typescript',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json'
+    },
+    rules: {}
+}
---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+import puppeteer from 'puppeteer'
+
+async function start (): Promise<void> {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto('https://substack.com/sign-in?redirect=%2F')
+}
+
+void start()
